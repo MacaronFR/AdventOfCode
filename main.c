d7128b1f16064c660c8a8b996f3b424ca375b13a
Denis Turbiez <denisft77@gmail.com>
2022-12-02 10:49:23 +0100

    advent of code 1.2

--- main.c
+++ main.c
@@ -8,19 +8,29 @@ int main() {
 	fseek(f, 0, SEEK_END);
 	long total = ftell(f);
 	fseek(f, 0, SEEK_SET);
-	int max = 0;
-	int current = 0;
+	long max1 = 0;
+	long max2 = 0;
+	long max3 = 0;
+	long current = 0;
 	while(ftell(f) != total){
 		memset(buffer, 0, 50);
 		fgets(buffer, 40, f);
 		if(buffer[0] == '\n'){
-			if(max < current){
-				max = current;
+			if(max1 < current){
+				max3 = max2;
+				max2 = max1;
+				max1 = current;
+			}else if(max2 < current){
+				max3 = max2;
+				max2 = current;
+			}else if(max3 < current){
+				max3 = current;
 			}
 			current = 0;
 		}else {
 			current += atoi(buffer);
 		}
 	}
-	printf("Max = %d", max);
+	printf("max1 = %ld, max2 = %ld, max3 = %ld", max1, max2, max3);
+	printf("total 3 max = %ld", max1 + max2 + max3);
 }
