743b4f5109f6621cf6c5a92465b575ab302cadd5
Denis Turbiez <denisft77@gmail.com>
2022-12-04 15:34:11 +0100

    advent of code 3.1

--- main.c
+++ main.c
@@ -1,27 +1,44 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include <ctype.h>
 
 int main() {
-	char buffer[50] = {0};
+	// READ FILE INIT
+	char buffer[100] = {0};
 	FILE *f = fopen("../data", "r");
 	fseek(f, 0, SEEK_END);
 	long total = ftell(f);
 	fseek(f, 0, SEEK_SET);
-	int o;
-	int m;
-	int res[3][3] = {
-			{3 + 0, 1 + 3, 2 + 6},
-			{1 + 0, 2 + 3, 3 + 6},
-			{2 + 0, 3 + 3, 1 + 6}
-	};
-	long score = 0;
+
+	//START CUSTOM VARIABLE
+	long sum = 0;
+	unsigned long len;
+	char saveL[26];
+	char saveU[26];
+
+	//START READ FILE
 	while(ftell(f) != total) {
-		memset(buffer, 0, 50);
-		fgets(buffer, 40, f);
-		o = buffer[0] - 'A';
-		m = buffer[2] - 'X';
-		score += res[o][m];
+		memset(buffer, 0, 100);
+		fgets(buffer, 90, f);
+
+		// START CODE
+		memset(saveL, 0, 26);
+		memset(saveU, 0, 26);
+		len = strlen(buffer);
+		for(int i = 0; i < len/2; ++i){
+			for(unsigned long j = len/2; j < len; ++j){
+				if(buffer[i] == buffer[j]){
+					if(isupper(buffer[i]) && saveU[buffer[i]-'A'] != 1){
+						saveU[buffer[i]-'A'] = 1;
+						sum += buffer[i] - 'A' + 27;
+					}else if(islower(buffer[i]) && saveL[buffer[i]-'a'] != 1){
+						saveL[buffer[i]-'a'] = 1;
+						sum += buffer[i] - 'a' + 1;
+					}
+				}
+			}
+		}
 	}
-	printf("%ld\n", score);
+	printf("%ld\n", sum);
 }
