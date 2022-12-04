540cea4ec570b0f43ee5b1f56e7a3a999f53978a
Denis Turbiez <denisft77@gmail.com>
2022-12-04 15:45:27 +0100

    advent of code 3.2

--- main.c
+++ main.c
@@ -3,6 +3,21 @@
 #include <stdlib.h>
 #include <ctype.h>
 
+int findItemInThree(char *elf1, char *elf2, char*elf3){
+	for(int i = 0; i < strlen(elf1); ++i){
+		for(int j = 0; j < strlen(elf2); ++j){
+			if(elf1[i] == elf2[j]){
+				for(int k = 0; k < strlen(elf3); ++k){
+					if(elf3[k] == elf2[j]){
+						return elf3[k];
+					}
+				}
+			}
+		}
+	}
+	return -1;
+}
+
 int main() {
 	// READ FILE INIT
 	char buffer[100] = {0};
@@ -12,33 +27,39 @@ int main() {
 	fseek(f, 0, SEEK_SET);
 
 	//START CUSTOM VARIABLE
+	char *elf1, *elf2, *elf3;
 	long sum = 0;
-	unsigned long len;
-	char saveL[26];
-	char saveU[26];
 
 	//START READ FILE
 	while(ftell(f) != total) {
 		memset(buffer, 0, 100);
 		fgets(buffer, 90, f);
 
-		// START CODE
-		memset(saveL, 0, 26);
-		memset(saveU, 0, 26);
-		len = strlen(buffer);
-		for(int i = 0; i < len/2; ++i){
-			for(unsigned long j = len/2; j < len; ++j){
-				if(buffer[i] == buffer[j]){
-					if(isupper(buffer[i]) && saveU[buffer[i]-'A'] != 1){
-						saveU[buffer[i]-'A'] = 1;
-						sum += buffer[i] - 'A' + 27;
-					}else if(islower(buffer[i]) && saveL[buffer[i]-'a'] != 1){
-						saveL[buffer[i]-'a'] = 1;
-						sum += buffer[i] - 'a' + 1;
-					}
-				}
-			}
+		elf1 = malloc(sizeof(char) * (strlen(buffer) + 1));
+		strcpy(elf1, buffer);
+
+		memset(buffer, 0, 100);
+		fgets(buffer, 90, f);
+
+		elf2 = malloc(sizeof(char) * (strlen(buffer) + 1));
+		strcpy(elf2, buffer);
+
+		memset(buffer, 0, 100);
+		fgets(buffer, 90, f);
+
+		elf3 = malloc(sizeof(char) * (strlen(buffer) + 1));
+		strcpy(elf3, buffer);
+
+		int res = findItemInThree(elf1, elf2, elf3);
+		if(islower(res)){
+			sum += res - 'a' + 1;
+		}else if(isupper(res)){
+			sum += res - 'A' + 27;
 		}
+
+		free(elf1);
+		free(elf2);
+		free(elf3);
 	}
 	printf("%ld\n", sum);
 }
