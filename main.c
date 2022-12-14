03a4e5cb84eaae3a55c0ce8350440aafb82e8dd2
Denis Turbiez <denisft77@gmail.com>
2022-12-14 21:49:08 +0100

    advent of code 10.2

--- main.c
+++ main.c
@@ -14,40 +14,35 @@ int main() {
 	fseek(f, 0, SEEK_SET);
 
 	//START CUSTOM VARIABLE
-	int cycle = 1;
+	int cycle = 0;
 	int tot = 0;
-	int x = 1;
+	int x = 0;
 	int add;
 	memset(buffer, 0, 110);
 
 	//START READ FILE
 	while(ftell(f) != total) {
-		cycle++;
 		if(buffer[0] == 0) {
 			memset(buffer, 0, 110);
 			fgets(buffer, 105, f);
 		}
+		if(cycle % 40 < x || cycle % 40 > x + 2){
+			printf(".");
+		}else{
+			printf("#");
+		}
+		if((cycle + 1) % 40 == 0){
+			printf("\n");
+		}
+		cycle++;
 		if(strncmp("noop", buffer, 4) == 0){
 			buffer[0] = 0;
-		}
-		if(buffer[0] == 1){
+		}else if(strncmp("addx", buffer, 4) == 0){
+			buffer[0] = 1;
+		}else if(buffer[0] == 1){
 			sscanf(buffer + 5, "%d", &add);
 			x += add;
 			buffer[0] = 0;
-		}
-		switch (cycle){
-			case 20:
-			case 60:
-			case 100:
-			case 140:
-			case 180:
-			case 220:
-				printf("X = %d\n", x);
-				tot += x * cycle;
-				break;
-		}
-		if(strncmp("addx", buffer, 4) == 0){
-			buffer[0] = 1;
 		}
 	}
 
